accfa991a60cbed571d88bc989fa9a819c8625a5
xmizutsuki <80939354+xmizutsuki@users.noreply.github.com>
2026-09-05 14:23:49 -0300

    Prepare ECG Lab Beta 1.1 release identity

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.ECG_CONFIG = {
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_k5vHw_F3u_JxaZmh6uumPQ_DPPy2EnU",
   ENABLE_DEMO_MODE: true,
   APP_NAME: "ECG Lab",
-  APP_VERSION: "1.0.0-beta.1"
+  APP_VERSION: "1.1.0-beta.1"
 };
